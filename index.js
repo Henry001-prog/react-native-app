955b7c03122921e855a2ecff3f7e84629a38a52c
Henry001-prog <alissonhenrique001@hotmail.com.br>
2020-01-28 11:25:04 -0300

    aplicaçao feita em react native utilizando o expo

--- index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import Router from './src/routes';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Router);
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,65 @@
-import Main from './pages/main';
-import User from './pages/user';
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { MaterialIcons } from "@expo/vector-icons";
 
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import Repos from "./pages/Repos";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
-const Routes = createStackNavigator(
+const Routes = createAppContainer(
+  createStackNavigator(
     {
-        Main: {
-            screen: Main,
+      Home: {
+        screen: Home,
+        navigationOptions: ({ navigation }) => ({
+          headerShown: false
+        })
+      },
+      Repos: {
+        screen: Repos,
+        navigationOptions: ({ navigation }) => ({
+          title: `Repos from ${navigation.getParam("username")}`,
+          headerBackTitleVisible: false,
+          headerStyle: {
+            shadowColor: "transparent",
+            backgroundColor: "#252535"
+          },
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ marginHorizontal: 8 }}
+              onPress={() =>
+                navigation.navigate("Profile", {
+                  username: navigation.getParam("username")
+                })
+              }
+            >
+              <MaterialIcons name="account-circle" size={26} color="#fff" />
+            </TouchableOpacity>
+          )
+        })
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          headerBackTitleVisible: false
+        }
+      }
+    },
+    {
+      initialRouteName: "Home",
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: "#252535"
         },
-        User: {
-            screen: User,
-        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold"
+        }
+      }
     }
+  )
 );
 
-const Router = createAppContainer(Routes);
-
-export default Router;
+export default Routes;
